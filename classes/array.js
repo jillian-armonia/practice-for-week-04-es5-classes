4dccbfce521b49a66230ebf0b571ee84d5ece155
Jillian Armonia <jillian.marice@gmail.com>
2024-07-02 13:23:02 +0900

    finished array class problem

--- classes/array.js
+++ classes/array.js
@@ -1,1 +1,8 @@
 // Your code here
+Array.prototype.isEqual = function(array){
+    const originalArray = this;
+    const result = originalArray.every((elem, i) => {
+        return array[i] === elem;
+    })
+    return result;
+}
